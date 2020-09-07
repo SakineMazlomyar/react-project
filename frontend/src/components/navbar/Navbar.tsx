bd7b79eec7597533d528d4ba1a9227fe90f5aca3
sakine mazlomyar <mazlomyar12@gmail.com>
2020-09-07 11:41:51 +0200

    modified navbar

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHashtag, faSignInAlt, faSignOutAlt, faBars } from '@fortawesome/free-solid-svg-icons';
 import { faTwitterSquare,faInstagram } from '@fortawesome/free-brands-svg-icons'
-import './navbar.css';
+
 
 interface Props {
     toggleOpenForm:()=>void,
@@ -10,50 +10,56 @@ interface Props {
   
 }
 interface State {
-    changeHeight:boolean
+   
 }
 export default class Navbar extends React.Component<Props, State>{
-    constructor(props:Props){
-        super(props);
-        this.state = {
-          changeHeight:false
 
-        }
-
-    }
-    handleChangeHeight = ()=>{ this.setState({changeHeight:!this.state.changeHeight})}
     render(){
         return(
-            <nav id={this.state.changeHeight?"heightNavbarCollaps":"heightNavbarNormal"} className="navbar fixed-top containerNavbar navbar-expand-sm ">
+
+            <nav className="navbar navbar-expand-md sticky-top navbar-dark">
                 <a className="navbar-brand" href="/">
-                    <FontAwesomeIcon icon={faHashtag} className="hashtagNavbar"/>
-                    <span className=" ml-2 titleNavbar font-weight-bold">Browse Skills</span>
-                </a>
-                <button className="navbar-toggler" data-toggle="collapse" data-target="#nav-menu">
-                    <FontAwesomeIcon icon={faBars}  className="navbar-toggler-icon mr-1" onClick={this.handleChangeHeight}/>
+                    <FontAwesomeIcon className="navbar-hashtag" icon={faHashtag}/>
+                    <span className="navbar-brand-name">Browse Skills</span>
+                </a>   
+                
+                <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#nav-menu">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="nav-menu">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item test"> <a href="/" className="nav-link pr-0">   <FontAwesomeIcon icon={faInstagram} className="socialMediaItemNavbar" /> </a> </li>
-                        <li className="nav-item"> <a href="/" className="nav-link  ">    <FontAwesomeIcon icon={faTwitterSquare}  className="socialMediaItemNavbar"/> </a> </li>
-                        <li className="nav-item d-flex  ml-2"> 
+                        <li className="nav-item"><a href="/" className="nav-link">   <FontAwesomeIcon icon={faInstagram} className="socialMediaItemNavbar-instagram" /> </a></li>
+                       <li> <a href="/" className="nav-link  ">    <FontAwesomeIcon icon={faTwitterSquare}  className="socialMediaItemNavbar-twitter"/> </a> </li>
+                        <li className="nav-item"> 
 
                         {this.props.openForm ? 
-
-                        <div className="d-flex justify-content-center align-items-center">
-                            <span className="text-center loginLogOut ">Logout</span>
-                            <FontAwesomeIcon icon={faSignOutAlt}   onClick={this.props.toggleOpenForm} className="loginLogOut loginLogOutIconSize "/>
+                         
+                        <div className="container  pr-0">
+                            <div className="row">
+                                <div className="col-6">
+                                    <span className="nav-sign-in-out">Logout</span>
+                                </div>
+                                <div className="col-6">
+                                    <FontAwesomeIcon className="nav-sign-in-out-icon" onClick={this.props.toggleOpenForm} icon={faSignOutAlt} />
+                                </div>
+                            </div>
+                               
                         </div>:
 
-                        <div className="d-flex justify-content-center align-items-center">
-                            <span className="text-center loginLogOut ">Login</span>
-                            <FontAwesomeIcon icon={faSignInAlt}   onClick={this.props.toggleOpenForm} className="loginLogOut loginLogOutIconSize "/>
+                        <div className="container pr-0">
+                            <div className="row">
+                                <div className="col-6 pl-0">
+                                    <span className="nav-sign-in-out">Login</span>
+                                </div>
+                                <div className="col-6 pl-0">
+                                <FontAwesomeIcon icon={faSignInAlt} className="nav-sign-in-out-icon"  onClick={this.props.toggleOpenForm}/>
+                                </div>
+                            </div> 
                         </div>}
                         </li>
                     </ul>
                 </div>
-             
             </nav>
         )
 
